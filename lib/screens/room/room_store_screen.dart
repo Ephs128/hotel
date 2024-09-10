8418cc3fa7d6b221bb8cfe597efb51f4846441d6
Ephs128 <nuggetflandes@gmail.com>
2024-09-09 23:36:24 -0400

    fix: room's store

--- lib/screens/room/room_store_screen.dart
+++ lib/screens/room/room_store_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:fluent_ui/fluent_ui.dart';
 import 'package:hotel/data/models/product_model.dart';
 import 'package:hotel/data/models/room_model.dart';
 import 'package:hotel/screens/room/rooms_screen.dart';
@@ -57,50 +57,49 @@ class _RoomStoreScreenState extends State<RoomStoreScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            HeaderFormWidget(
-              back: () => backFunction(), 
-              title: title,
-            ),
-            Row(
-              children: [
-                Column(
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          HeaderFormWidget(back: () => backFunction(), title: title),
+          const SizedBox(height: 20,),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
                   children: [
                     Table(
-                      border: TableBorder.all(),
-                      columnWidths: const {
-                        0: FlexColumnWidth(),
-                        1: FixedColumnWidth(50),
-                        2: FixedColumnWidth(50),
-                      },
-                      children: [
-                        TableRow(
-                          decoration: BoxDecoration(color: Colors.grey[120]),
-                          children: const [
-                            HeaderTableCell(text: "Nombre"),
-                            HeaderTableCell(text: "Cantidad"),
-                            HeaderTableCell(text: "Precio"),
-                          ]
-                        ),
-                        for (List<String> data in productsInOrder) 
+                        border: TableBorder.all(),
+                        columnWidths: const {
+                          0: FlexColumnWidth(),
+                          1: FixedColumnWidth(60),
+                          2: FixedColumnWidth(60),
+                        },
+                        children: [
                           TableRow(
-                            decoration: BoxDecoration(color: Colors.grey[20]),
-                            children: [
-                              NormalTableCell(child: Text(data[0])), // todo: cambiar por los datos reales
-                              NormalTableCell(child: Text(data[1])), // todo: cambiar por los datos reales
-                              NormalTableCell(child: Text(data[2])), // todo: cambiar por los datos reales
+                            decoration: BoxDecoration(color: Colors.grey[120]),
+                            children: const [
+                              HeaderTableCell(text: "Nombre"),
+                              HeaderTableCell(text: "Cantidad"),
+                              HeaderTableCell(text: "Precio"),
                             ]
                           ),
-                      ],
+                          for (List<String> data in productsInOrder) 
+                            TableRow(
+                              decoration: BoxDecoration(color: Colors.grey[20]),
+                              children: [
+                                NormalTableCell(child: Text(data[0])), // todo: cambiar por los datos reales
+                                NormalTableCell(child: Text(data[1])), // todo: cambiar por los datos reales
+                                NormalTableCell(child: Text(data[2])), // todo: cambiar por los datos reales
+                              ]
+                            ),
+                        ],
                     ),
                     Table(
                       border: TableBorder.all(),
                       columnWidths: const {
                         0: FlexColumnWidth(),
-                        1: FixedColumnWidth(50),
+                        1: FixedColumnWidth(60),
                       },
                       children: [
                         TableRow(
@@ -114,22 +113,32 @@ class _RoomStoreScreenState extends State<RoomStoreScreen> {
                     ),
                   ],
                 ),
-                Column(
-                  children: [
-                    const Text("Productos:"),
-                    ListView(
-                      children: productsAvilable.map((product) {
-                        return ListTile(
-                          title: Text(product.toString()),
-                        );
-                      }).toList()
-                    )
-                  ],
-                )
-              ],
-            )
-          ],
-        ),
+              ),
+              const SizedBox(width: 30,),
+              Container(
+                decoration: BoxDecoration(border: Border.all()),
+                child: SizedBox(
+                  height: MediaQuery.sizeOf(context).height - 200,
+                  width: MediaQuery.sizeOf(context).width / 4,
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: productsAvilable.map((product) {
+                      return ListTile(
+                        title: Text(product.toString()),
+                      );
+                    }).toList()
+                  ),
+                ),
+              ),
+          
+            ],
+          ),
+          const SizedBox(height: 30,),
+          FilledButton(
+            child: const Text("Guardar"), 
+            onPressed: () {}
+          )
+        ],
       ),
     );
   }
